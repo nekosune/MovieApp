463e58da71a3827dd41fb44dc03f98c5ad871237
Katrina Swales <kat.swales@nekokittygames.com>
2015-08-24 19:43:36 +0100

    Added other files

---
+++ app/src/main/java/com/nekokittygames/movieapp/MovieAPI.java
@@ -0,0 +1,16 @@
+package com.nekokittygames.movieapp;
+
+import android.app.Activity;
+
+/**
+ * Created by Katrina on 24/08/2015.
+ */
+public class MovieAPI {
+
+
+    public String testApplication(Activity act)
+    {
+        String apiKey=act.getString(R.string.movie_api_key);
+
+    }
+}
